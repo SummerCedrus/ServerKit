ff7d93e7d18db14cc3a49f18c03d9065d4812ee3
SummerCedrus <876110304@qq.com>
2018-06-12 20:59:48 +0800

    add grpc

--- make_proto.sh
+++ make_proto.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd protocol
-protoc --go_out=. protocol.proto
-mv protocol.pb.go ../src/protocol 
+protoc protocol/protocol.proto --go_out=src/
+#mv protocol.pb.go ../src/protocol 
+protoc protocol/rpc.proto --go_out=plugins=grpc:src/
